bd87a72f6c24cebcf77705d69815443ef766acc7
KIMBERLYSEDDON <82526676+KIMBERLYSEDDON@users.noreply.github.com>
2021-06-22 16:03:50 -0400

    added stater code and questions array

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your GitHub username?", "What is your email address?", "What is your project's name?", "Write a short description of your project", "Which license should your project have?", "What command should be run to tun tests?", "What does the user need to know about using this repo?", "What does the user need to know about contributing to the repo?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
